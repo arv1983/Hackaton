db29afabec1323fef9c0743c2c8a751ab2078c4d
Anderson Vasconcelos <ciclista-rs@hotmail.com>
2021-04-27 20:00:07 -0300

    hackaton

--- hackathon.py
+++ hackathon.py
@@ -2,11 +2,8 @@ hackathon_1 = ["Team Kenzie", "Team Ateliware", "Team VHSYS", "Team Mirum"]
 hackathon_2 = ["Team Ateliware", "Team Kenzie", "Team VHSYS", "Team Mirum"]
 hackathon_3 = ["Team Mirum", "Team Ateliware","Team VHSYS", "Team Kenzie"]
 
-
 def get_score(team_name, teams):
-    index = teams.index(team_name)
-    print('O ' + team_name + 'ficou em classificada em ', teams.index(team_name) + 1, 'lugar')
-    
+    return 'O ' + team_name + 'ficou classificada em ' + str(teams.index(team_name) + 1) + ' lugar'
 
 get_score_1 = get_score("Team Ateliware", hackathon_1)
 print(get_score_1)
